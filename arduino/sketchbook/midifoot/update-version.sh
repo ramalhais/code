4e4505d9ee271b3fd9ce958574a2b7f9179fa57c
Pedro Ramalhais <pedro.ramalhais@sky.uk>
2016-12-06 01:27:24 +0000

    Update OTA version.

--- arduino/sketchbook/midifoot/update-version.sh
+++ arduino/sketchbook/midifoot/update-version.sh
@@ -5,3 +5,4 @@ echo "$MIDIFOOT_VERSION" > midifoot-latest.txt
 cp /C/Users/ramalhais/AppData/Local/Temp/arduino_build_358381/midifoot.ino.bin midifoot-$MIDIFOOT_VERSION.bin
 git add --all
 git commit -a -m "Update OTA version."
+git push
